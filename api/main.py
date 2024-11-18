0cb6527d1c17afe06570e97bc6e714588292ac3e
N.B.A. Taken-Koevoets <ninataken2@hotmail.com>
2024-11-18 14:38:17 +0100

    Test Driven Development Setup
    + Pytest install
    + Test workflow for GitHub
    + initial test (test_root)

--- api/main.py
+++ api/main.py
@@ -12,5 +12,7 @@ app.include_router(users.router)
 @app.get("/")
 async def root():
     return {
-        "message": "Welcome to the Sanquin API! \n Visit /docs for the API documentation."
+        200: {
+            "message": "Welcome to the Sanquin API! \n Visit /docs for the API documentation."
+        }
     }
---
+++ api/tests/test_main.py
@@ -0,0 +1,12 @@
+from fastapi.testclient import TestClient
+from api.main import app
+
+client = TestClient(app)
+
+
+def test_root():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {
+        "message": "Welcome to the Sanquin API! \n Visit /docs for the API documentation."
+    }
