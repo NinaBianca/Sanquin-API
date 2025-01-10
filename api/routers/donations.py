96c8ba73c200d208928e4f5f108e534a975bf187
N.B.A. Taken-Koevoets <ninataken2@hotmail.com>
2025-01-10 18:51:53 +0100

    Update donations.py

--- api/routers/donations.py
+++ api/routers/donations.py
@@ -108,7 +108,7 @@ def get_location_info_by_city_route(city: str, db: Session = Depends(get_db)):
 def get_location_name_by_id_route(location_id: int, db: Session = Depends(get_db)):
     try:
         location = get_location_name_by_id(db, location_id)
-        return ResponseModel(status=200, data=str(location), message="Location retrieved successfully")
+        return ResponseModel(status=200, data={"name": location}, message="Location retrieved successfully")
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"An error occurred while retrieving location information: {e}") from e
 
