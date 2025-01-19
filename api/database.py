601ce661bd0a04784d33ae1f841fb947204f7fc3
N.B.A. Taken-Koevoets <ninataken2@hotmail.com>
2025-01-19 16:35:42 +0100

    Update database.py
    increase pool size

--- api/database.py
+++ api/database.py
@@ -13,7 +13,12 @@ POSTGRES_SERVER = os.getenv("POSTGRES_SERVER")
 if not POSTGRES_SERVER:
     raise ValueError("POSTGRES_SERVER is not set")
 
-engine = create_engine(POSTGRES_SERVER)
+engine = create_engine(
+    POSTGRES_SERVER,
+    pool_size=10,
+    max_overflow=20,
+    pool_timeout=30,
+    pool_recycle=1800,)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
